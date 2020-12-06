0465d9497ef1ce3c72fe0972b294a1cc754f10ee
TalYachini <yachini@post.bgu.ac.il>
2020-12-06 23:36:56 +0200

    HW6 cv was updated

---
+++ CV_Tal_Yachini/ScriptCV.js
@@ -0,0 +1,15 @@
+function SubmitContact(){
+    // alert("Thank you! We submitted your contact request");
+    document.getElementById("ContactForm").innerHTML = "Thank For Your Submmition! I Will Contact You Soon";
+    document.getElementById("form");
+}
+function mouseOver(x){
+
+    x.style.fontWeight= 'bold';
+    x.style.color = 'mediumvioletred';
+}
+
+function mouseOut(x){
+    x.style.fontWeight= 'normal';
+    x.style.color = "black"
+}
