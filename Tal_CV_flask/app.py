e46c99a1b129f1b3c5ab2d2b16cde0dd90002eed
TalYachini <yachini@post.bgu.ac.il>
2020-12-26 12:29:41 +0200

    CV as flask project- update

--- Tal_CV_flask/app.py
+++ Tal_CV_flask/app.py
@@ -4,9 +4,27 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def Tal_CV():
+    return render_template('CV_Tal_Yachini.html')
+
+@app.route('/assignment8')
+def My_Hobbies():
+    myHobbies = ['Dancing', 'Swimming', 'Traveling']
+    return render_template('assignment8.html', hobbies = myHobbies)
+
+@app.route('/users_list')
+def My_Users_List():
+    return render_template('LIST_USERS.html')
+
+@app.route('/header')
+def myHeader():
+    return render_template('header.html')
+
+@app.route('/MyMusic')
+def MyMusic():
+    myFaveSongs = {'Kansas' : 'Dust in the Wind' , 'Shalom Hanoch' : 'Layla' , 'Coldplay' : 'The Scientist ' , 'Amy Winehouse' : 'Back to Black'}
+    return render_template('MyMusic.html' , FaveMusic = myFaveSongs)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- flaskProject1/app.py
+++ flaskProject1/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, redirect,url_for
+from flask import Flask, redirect,url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index_func():
+    #you get the name
+    username= ''
+    return render_template('index2.html',name = username)
 
 @app.route('/Tal')
 def about_func():
@@ -9,8 +14,8 @@ def about_func():
 
 @app.route('/home')
 @app.route('/')
-def home():
-    return 'Welcome to my Website'
+def home_func():
+    return render_template('home.html' , user = {'firstName' : 'Ariel' , 'lastName' : 'Perchik'}, hobbies = ['dancing, teaching'], degree = ('B.Sc' , 'M.Sc'))
 
 @app.route('/check')
 def check():
@@ -23,6 +28,12 @@ def check():
 @app.route('/other')
 def other():
     return redirect('/Tal')
+
+
+@app.route('/user')
+def user_func():
+    user_from_DB = {'firstName' : 'Ariel' , 'lastName' : 'Perchik' , 'gender' :'boy'}
+    return render_template('user.html', user = user_from_DB, hobbies = ['dancing, teaching'])
 
 if __name__ == '__main__':
     app.run(debug=True)
